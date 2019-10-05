52de407717238aa14e94081ba7bb8a366f09a73f
Kshitij <kshitij@kritter.in>
2019-10-06 03:37:06 +0530

    Added kritter.framework

---
+++ kritter.framework/Headers/kritter.h
@@ -0,0 +1,19 @@
+//
+//  kritter.h
+//  kritter
+//
+//  Created by Nikit Phadke on 9/17/19.
+//  Copyright © 2019 Kritter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for kritter.
+FOUNDATION_EXPORT double kritterVersionNumber;
+
+//! Project version string for kritter.
+FOUNDATION_EXPORT const unsigned char kritterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kritter/PublicHeader.h>
+
+
